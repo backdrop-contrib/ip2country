546549c9c585a3d513d90d41bdd68f5e1305aa49
Tim Rohaly <tr@202830.no-reply.drupal.org>
2009-02-18 00:53:11 +0000

    Initial commit of ip2country module

---
+++ uc_ip2country.js
@@ -0,0 +1,36 @@
+if (Drupal.jsEnabled) {
+  $(document).ready(function() {
+    // Remove focus from the "Update" button
+    $('#uc-ip2country-admin-settings .form-submit').eq(1).focus();
+    var selectCountry = function() {
+        $('#edit-uc-ip2country-test-country').removeAttr('disabled');
+        $('#edit-uc-ip2country-test-ip-address').attr('disabled', 'disabled');
+    }
+    var selectIP = function() {
+        $('#edit-uc-ip2country-test-country').attr('disabled', 'disabled');
+        $('#edit-uc-ip2country-test-ip-address').removeAttr('disabled');
+    }
+    // Select the appropriate input based on radio button values
+    // First time in...
+    var j = $(".form-radios").find("input:radio").eq(0).attr('checked');
+    if (j) selectCountry(); else selectIP();
+    // Subsequent clicks...
+    $(".form-radios").find("input:radio").click(function() {
+      // As per Lyle, choosing to use click because of IE's stupid bug not to
+      // trigger onChange until focus is lost.
+      if ($(this).val() == 0) selectCountry(); else selectIP();
+    });
+    // When "Update" button is pushed, make an AJAX call to initiate the
+    // database update without leaving this page.  Throw up a progress
+    // marker because it takes a long time.
+    $('#edit-uc-ip2country-update-database').click(function(){
+      var databaseUpdated = function(data) {
+        var result = Drupal.parseJson(data);
+        $('#dbthrobber').removeAttr('style').html(result['message'] + result['count']).attr('style', "align:left; color:green;");
+      }
+      $('#dbthrobber').attr('style', 'background-image: url(' + Drupal.settings['base_path'] + 'misc/throbber.gif); background-repeat:no-repeat; background-position:100% -20px; align:left; color:green;').html('Working...&nbsp;&nbsp;&nbsp;&nbsp;');
+      $.get(Drupal.settings['base_path'] + 'admin/store/settings/ip2country/update', null, databaseUpdated);
+      return false;
+    });
+  });
+}
