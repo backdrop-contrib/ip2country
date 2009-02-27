fa792157eee78ba8e477f5ad2028a0295997ae4e
Tim Rohaly <tr@202830.no-reply.drupal.org>
2009-02-27 04:41:19 +0000

    Add menu option to select which RIR to get data from.

--- uc_ip2country.js
+++ uc_ip2country.js
@@ -26,7 +26,7 @@ if (Drupal.jsEnabled) {
     $('#edit-uc-ip2country-update-database').click(function(){
       var databaseUpdated = function(data) {
         var result = Drupal.parseJson(data);
-        $('#dbthrobber').removeClass('working').html(result['message'] + result['count']).addClass('completed');
+        $('#dbthrobber').removeClass('working').html(result['message'] + '  ' + result['count']).addClass('completed');
       }
       $('#dbthrobber').removeClass('message completed').addClass('working').html('Working...&nbsp;&nbsp;&nbsp;&nbsp;');
       $.get(Drupal.settings['base_path'] + 'admin/store/settings/ip2country/update', null, databaseUpdated);
